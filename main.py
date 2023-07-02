9f5dc74e239fdb77b4ac5dbd70bbf8eb6e9f5e38
MarcelDRM <122673740+MarcelDRM@users.noreply.github.com>
2023-07-02 09:23:43 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-secret_number = 9
-guess_count = 0
-guess_limit = 3
-while guess_count < guess_limit:
-    guess = int(input('Guess: '))
-    guess_count += 1
-    if guess == secret_number:
-        print('You Win')
+command = ""
+started = False
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        if started:
+            print("Car already started")
+        else:
+            started = True
+            print("Car started..")
+    elif command == "stop":
+        if not started:
+            print("Car already stopped")
+        else:
+            started = False
+            print("Car stoped")
+    elif command == "help":
+        print(""""
+start - to start the car
+stop - to stop the car
+quit - to exit game        
+        """)
+    elif command == "quit":
         break
-else:
-    print('Sorry you failed')
+    else:
+        print("I don't understand")
